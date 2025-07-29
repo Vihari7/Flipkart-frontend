0abee2c2588a25097271adec9c786eb3eaa458e0
Vihari Gunarathna <106915486+Vihari7@users.noreply.github.com>
2025-07-29 13:43:44 +0530

    Create Header.js

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+function Header() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">Flipkart</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
+export default Header;
